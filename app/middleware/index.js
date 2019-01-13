541a0822e73f5ab47035bf19dc9b79e594d86da9
Sungbin Jo <pcr910303@icloud.com>
2019-01-13 13:40:13 +0900

    Change mongodb connection persistently for faster responses

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,9 +1,15 @@
-const { MongoClient, ObjectId } = require("mongodb");
+const logger = require("../logger.js");
+const { MongoClient } = require("mongodb");
+const url = `mongodb+srv://${process.env.PEBBLE_DB_USER}:${process.env.PEBBLE_DB_PW}@csi-web-db-nvdgx.mongodb.net/test?retryWrites=true`;
+let client = null;
+(async () => {
+    client = await MongoClient.connect(url, { useNewUrlParser: true });
+    logger.info("Database connection established");
+})();
 
 module.exports = {
     getDB: async (ctx, next) => {
-        const url = `mongodb://${process.env.PEBBLE_DB_USER}:${process.env.PEBBLE_DB_PW}@ds149744.mlab.com:49744/csi-web-db`;
-        ctx.state.client = await MongoClient.connect(url, { useNewUrlParser: true });
+        ctx.state.client = client;
         ctx.state.db = ctx.state.client.db("csi-web-db");
         ctx.state.collection = {};
         ctx.state.collection.users = ctx.state.db.collection("users");
