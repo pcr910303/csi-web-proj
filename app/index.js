ba0aff4d8b58c739745476dba3497effbe7954c0
Sungbin Jo <pcr910303@icloud.com>
2019-01-13 13:37:40 +0900

    Logging request, response, and errors

--- app/index.js
+++ app/index.js
@@ -9,6 +9,18 @@ const PORT = 8002;
 
 app.use(endpoint.routes()).use(endpoint.allowedMethods());
 
-const server = app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.use(async (ctx, next) => {
+    ctx.state.logger = logger;
+    logger.info(ctx.method, ctx.url);
+    await next();
+    logger.info(ctx.body);
+});
+
+app.on("error", (err, ctx) => {
+    logger.error(err);
+    logger.info(ctx);
+});
+
+const server = app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
 
 module.exports = server;
