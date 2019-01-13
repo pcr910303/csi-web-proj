206a026e6bf22bcdcecd7d22257b048f00495e16
Sungbin Jo <pcr910303@icloud.com>
2019-01-13 13:42:47 +0900

    Add timetable initalization when connecting db

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -5,6 +5,13 @@ let client = null;
 (async () => {
     client = await MongoClient.connect(url, { useNewUrlParser: true });
     logger.info("Database connection established");
+    const times = client.db("csi-web-db").collection("times");
+    (new Array(100)).fill().map((currentValue, index) => {
+        times.findOneAndUpdate({ times: index }, { $setOnInsert: {
+            times: index
+        } }, { upsert: true });
+    });
+    logger.info("Database collection times initalized");
 })();
 
 module.exports = {
